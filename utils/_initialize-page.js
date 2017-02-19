9c98b5fbb0d8fb9f908cc5db951d385cb8855089
Kittipon <heartmon12@gmail.com>
2017-02-19 19:17:16 +0100

    again...

--- utils/_initialize-page.js
+++ utils/_initialize-page.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Provider } from 'mobx-react';
-import { initBaseStore, initAuthStore } from '../stores';
+// import { initBaseStore, initAuthStore } from '../stores';
 import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
@@ -19,8 +19,8 @@ export default function initializePage(ComposedComponent) {
             const { req } = ctx;
             const isServer = !!req;
             const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
-            const baseStore = initBaseStore(isServer);
-            const authStore = initAuthStore(isServer);
+            // const baseStore = initBaseStore(isServer);
+            // const authStore = initAuthStore(isServer);
 
             let pageProps = {};
             if (ComposedComponent.getInitialProps) {
@@ -37,12 +37,12 @@ export default function initializePage(ComposedComponent) {
 
         constructor(props) {
             super(props);
-            this.baseStore = initBaseStore(props.isServer, props.helloMessage);
+            // this.baseStore = initBaseStore(props.isServer, props.helloMessage);
         }
 
         render() {
             return (
-                <Provider BaseStore={this.baseStore} AuthStore={this.authStore}>
+                <Provider>
                   <MuiThemeProvider muiTheme={getMuiTheme({userAgent: this.props.userAgent})}>
                         <ComposedComponent {...this.props}
                         />
