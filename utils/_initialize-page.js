f128bc7ad52c47893d14336e4339c89753c5b9b5
Kittipon <heartmon12@gmail.com>
2017-02-19 19:08:08 +0100

    fix dependency issue

--- utils/_initialize-page.js
+++ utils/_initialize-page.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { Provider } from 'mobx-react';
-import { initBaseStore, initAuthStore } from '~/stores';
+import { initBaseStore, initAuthStore } from '../stores';
 import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
